014ff05f8ca2ef5c82beac2ce9116ee580214d5b
Oscar Julian Sosa Ortiz <okijulian0791@gmai.com>
2024-10-06 22:47:17 -0300

    - se separa reorganiza  y crea una Extensions/ServiceExtensions.cs para poner la logica que tenia en el Program.cs

--- ClinicaSeprice/Program.cs
+++ ClinicaSeprice/Program.cs
@@ -1,54 +1,17 @@
-using ClinicaSepriceAPI.Data;
-using ClinicaSepriceAPI.Interfaces;
-using ClinicaSepriceAPI.Repositories;
-using ClinicaSepriceAPI.Services;
-using ClinicaSepriceAPI.Helpers;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using ClinicaSepriceAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Validar configuraciones JWT
-JwtHelper.ValidarConfiguracionJWT(builder.Configuration);
+builder.Services.ConfigureJWT(builder.Configuration);
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// Inyectar el UsuarioService
-builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-builder.Services.AddScoped<UsuarioService>();
-
-// Configurar autenticación JWT
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
-
-// Configurar DbContext para MySql
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 2))));
+// Configuración de servicios y dependencias
+builder.Services.ConfigureServices();
+builder.Services.ConfigureDbContext(builder.Configuration);
 
 var app = builder.Build();
 
-// Configuración para Swagger en entornos de desarrollo y producción
+// Configuración de Swagger para entornos de desarrollo y producción
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -64,14 +27,10 @@ else
     });
 }
 
-// Middleware para manejar la autenticación
-app.UseAuthentication();
-
-// Middleware para manejar autorización
-app.UseAuthorization();
-
-// Redirección a HTTPS
+// Middleware
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Mapear los controladores de la API
 app.MapControllers();
