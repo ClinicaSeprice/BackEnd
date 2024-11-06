a57cfa5555237a6e5c029024ca33adcec5ad1a8d
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2024-11-06 19:51:32 -0300

    creación Historia clinica DTO, interfaz, service. Peleando con controller

---
+++ ClinicaSeprice/Controllers/HistoriaClinicaController.cs
@@ -0,0 +1,59 @@
+﻿using ClinicaSepriceAPI.DTOs;
+using ClinicaSepriceAPI.Interfaces;
+using ClinicaSepriceAPI.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaSepriceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HistoriaClinicaController : ControllerBase
+    {
+        private readonly IHistoriaClinicaService _HistoriaClinicaService;
+
+        public HistoriaClinicaController(IHistoriaClinicaService HistoriaClinicaService)
+        {
+            _HistoriaClinicaService = HistoriaClinicaService;
+        }
+
+        // Registro de Obra Social 
+
+        [HttpPost("altaHistoriaClinica")]
+        public async Task<IActionResult> AltaHistoriaClinica([FromBody] HistoriaClinicaDTO HistoriaClinicaDTO)
+        {
+            try
+            {
+                var registroExitoso = await _HistoriaClinicaService.RegistrarHistoriaClinicaAsync(HistoriaClinicaDTO);
+                if (!registroExitoso)
+                {
+                    return BadRequest("El registro de la obra social falló.");
+                }
+                return Ok("Obra Social registrada exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("buscarHistoriaClinicaPorId/{id}")]
+        public async Task<IActionResult> ObtenerHistoriaClinicaPorId(int id)
+        {
+            try
+            {
+                var HistoriaClinica = await _HistoriaClinicaService.ObtenerHistoriaClinicaPorIdAsync(id);
+                if (!HistoriaClinica?.Any() ?? true)
+                {
+                    return NotFound($"No se encontró ninguna historia clínica con Id {id}");
+                }
+                return Ok(HistoriaClinica);
+            }
+            catch
+            (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ ClinicaSeprice/Interfaces/IHistoriaClinicaService.cs
@@ -0,0 +1,11 @@
+﻿using ClinicaSepriceAPI.DTOs;
+
+namespace ClinicaSepriceAPI.Interfaces
+{
+    public interface IHistoriaClinicaService
+    {
+        Task<bool> RegistrarHistoriaClinicaAsync(HistoriaClinicaDTO historiaClinicaDto);
+
+
+    }
+}
