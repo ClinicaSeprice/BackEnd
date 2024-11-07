9de8c9d20913fca3abc602401f938e7baff52fb9
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2024-11-07 00:06:16 -0300

    creación de interfaz para Horarios disponibles

---
+++ ClinicaSeprice/Interfaces/IHorarioDisponible.cs
@@ -0,0 +1,10 @@
+﻿using ClinicaSepriceAPI.DTOs;
+
+namespace ClinicaSepriceAPI.Interfaces
+{
+    public interface IHorarioDisponible
+    {
+        Task<bool> RegistrarHorarioDisponibleAsync(HorarioDisponibleDTO horarioDisponibleDTO);
+        Task<IEnumerable<HorarioDisponibleDTO>> ObtenerHorarioDisponibleAsync(int id);
+    }
+}
