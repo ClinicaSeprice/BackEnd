a94ce506f6cc8c2658bbf14f71c3d89c209a92ce
Alexa-2k <109705749+Alexa-2k@users.noreply.github.com>
2024-11-07 22:17:50 -0300

    ajustes finales Historias clinicas

--- ClinicaSeprice/Interfaces/IHistoriaClinicaService.cs
+++ ClinicaSeprice/Interfaces/IHistoriaClinicaService.cs
@@ -1,10 +1,13 @@
 ﻿using ClinicaSepriceAPI.DTOs;
+using ClinicaSepriceAPI.Models;
+using System.Threading.Tasks;
 
 namespace ClinicaSepriceAPI.Interfaces
 {
     public interface IHistoriaClinicaService
     {
-        Task<bool> ObtenerHistoriaClinicaPorIdAsync(int id);
+        //Task<bool> ObtenerHistoriaClinicaPorIdAsync(int id);
+        Task<HistoriaClinica> ObtenerHistoriaClinicaPorIdAsync(int id);
         Task<bool> RegistrarHistoriaClinicaAsync(HistoriaClinicaDTO historiaClinicaDto);
 
 
